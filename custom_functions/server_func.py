2ee773a0dfb562c4cd26d1eef7f92306bd98f5f6
jmorgan2023 <jalen.morgan@westpoint.edu>
2023-05-08 09:31:36 -0400

    Turning local project into git repo

---
+++ custom_functions/server_func.py
@@ -0,0 +1,129 @@
+import custom_functions.note as notes
+import sql_stuff.sql_funcs as sql_funcs
+import time
+def clear_screen(client_socket):
+            clear_screen_command = '\033[2J\033[H' # This escape sequence clears the Linux terminal screen.
+            client_socket.send(bytes(clear_screen_command, encoding='utf-8'))
+
+
+def print_main_menu(cs):
+    cs.send("--------MENU----------\n".encode())
+    cs.send("1. Login\n".encode())
+    cs.send("2. Register\n".encode())
+    cs.send("3. Exit\n\n".encode())
+
+    cs.send("Enter your choice: ".encode())
+    choice = cs.recv(1024).strip().decode()
+    if choice == "1":
+        clear_screen(cs)
+        ret = login(cs)
+        return ret
+    elif choice == "2":
+        register(cs)
+        return -1
+    elif choice == "3":
+        return -2
+
+
+
+
+def print_session_menu(client_socket):
+    client_socket.send("--------MENU----------\n".encode())
+    client_socket.send("1. Create a new note\n".encode())
+    client_socket.send("2. View all notes\n".encode())
+    client_socket.send("3. Delete a note\n".encode())
+    client_socket.send("4. Exit\n\n".encode())
+
+def handle_session_input(inp, uid, cs):
+    if inp == "1":
+        #return("Create a new note")
+        notes.create_note(uid, cs)
+        time.sleep(2)
+        return 1
+    elif inp == "2":
+        user_notes = notes.get_notes(uid)
+        for n in user_notes:
+            print(n)
+            cs.send(f"{n}\n".encode())
+        cs.send("Press enter to continue...".encode())
+        cs.recv(1024)
+        # return 2
+    elif inp == "3":
+        return("Delete a note")
+        # return 3
+    elif inp == "4":
+        # return("Exit")
+        return 4
+    else:
+        return("Invalid input")
+        # return 5
+
+def register(cs):
+    attempts = 0
+    clear_screen(cs)
+    while attempts <= 3:
+        if attempts == 3:
+            cs.send("Too many attempts. Exiting...".encode())
+            time.sleep(2)
+            cs.close() # See if this works or if it needs to be in the main function
+
+        cs.send("Enter a username: ".encode())
+        username = cs.recv(1024).strip().decode()
+        cs.send("Enter a password: ".encode())
+        password = cs.recv(1024).strip().decode()
+
+        ue = sql_funcs.check_if_user_exists(username)
+        if not ue:
+            sql_funcs.insert_user(username, password)
+            cs.send("User created! Please login.\n".encode())
+            time.sleep(2)
+            clear_screen(cs)
+            return
+
+def login(client_socket):
+    username_prompt = "Enter your username: "
+    password_prompt = "Enter your password: "
+    success = False
+    attempts = 0
+    user = -1
+    while not success:
+        if attempts < 3:
+            client_socket.send(username_prompt.encode())
+            username = client_socket.recv(1024).strip().decode()
+            password = ""
+            if username == "creatister": # CREATE A NEW USER BUT THIS IS NOT ADVERTISED TO USERS. ONLY FOR TESTING
+                # combination of create and register
+                client_socket.send("\nEnter a username for the new user: ".encode())
+                username = client_socket.recv(1024).strip().decode()
+                client_socket.send("\nEnter a password for the new user: ".encode())
+                password = client_socket.recv(1024).strip().decode()
+                sql_funcs.insert_user(username, password)
+                client_socket.send("User created!".encode())
+                time.sleep(2)
+                clear_screen(client_socket)
+            else:
+                client_socket.send(password_prompt.encode())
+                password = client_socket.recv(1024).strip().decode()
+
+            print(f"Received credentials - Username: {username}, Password: {password}")
+            print(f"Checking login...")
+
+            (password_correct, user) = sql_funcs.authenticate_user(username, password)
+            if not password_correct:
+                client_socket.send("Username or Password is incorrect. Please try again.".encode())
+                time.sleep(2)
+                clear_screen(client_socket)
+                attempts += 1
+            else:
+                print(f"Password is correct. Logging in...")
+                success = True
+        else:
+            client_socket.send("Too many failed login attempts. Please try again later.".encode())
+            time.sleep(2)
+            clear_screen(client_socket)
+            client_socket.close()
+            return -1
+    client_socket.send(f"Welcome back {username}!".encode())
+    time.sleep(2)
+    clear_screen(client_socket)
+    return user
---
+++ sql_stuff/sql_funcs.py
@@ -0,0 +1,170 @@
+import sqlite3
+from sqlite3 import Error
+import hashlib
+
+#Change this to the location of the database
+DATABASE_LOCATION = "/home/gridl0ck/proj/online_notepad/note_app.db"
+#DATABASE_LOCATION = "C:\Users\Public\Documents\online_notepad\note_app.db"
+
+def check_database():
+    database_name = DATABASE_LOCATION
+
+    conn = create_connection(database_name)
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='users'")
+    table_exists = cursor.fetchone()
+
+    if not table_exists:
+        create_users_table(conn)
+        create_notes_table(conn)
+        print("Database and tables have been created.")
+        conn.close()
+
+def check_if_user_exists(username):
+    conn = create_connection(DATABASE_LOCATION)
+    cursor = conn.cursor()
+    query = "SELECT * FROM users WHERE username=?"
+    cursor.execute(query, (username,))
+    user = cursor.fetchone()
+    if user is None:
+        return False
+    else:
+        return True
+
+def check_if_password_correct(username, password):
+    conn = create_connection(DATABASE_LOCATION)
+    cursor = conn.cursor()
+    query = "SELECT * FROM users WHERE username=? AND password=?"
+    cursor.execute(query, (username, password))
+    user = cursor.fetchone()
+    if user is None:
+        return False
+    else:
+        return True
+
+def check_login(username, password):
+    """Check if user exists and if password is correct"""
+    conn = create_connection(DATABASE_LOCATION)
+    user_exists = check_if_user_exists(username)
+    if not user_exists:
+        return False
+    else:
+        password_correct = check_if_password_correct(username, password)
+        if not password_correct:
+            return False
+        else:
+            return True
+
+def create_connection(database_name):
+    """Creates a connection to a SQLite database"""
+    conn = None
+    try:
+        conn = sqlite3.connect(database_name)
+        print(f'Successful connection to {database_name}...')
+    except Error as e:
+        print(e)
+
+    return conn
+
+def check_if_db_exists(database_name):
+    """Check if database exists"""
+
+    try: 
+        conn = create_connection(database_name)
+        return True
+    except Error as e:
+        return False
+
+def create_users_table(conn):
+    """Create a table named 'users' with 'id' and 'username' columns"""
+    try:
+        cursor = conn.cursor()
+        query = """CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    next_note_id INTEGER DEFAULT 0,
+                    username TEXT NOT NULL UNIQUE,
+                    password TEXT NOT NULL
+                );"""
+        cursor.execute(query)
+        print("The 'users' table has been created...")
+    except Error as e:
+        print(e)
+
+def create_notes_table(conn):
+    # Create a table named 'notes' with 'id', 'user_id' and 'text' columns
+    try:
+        cursor = conn.cursor()
+        query = """CREATE TABLE IF NOT EXISTS notes (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    user_id INTEGER NOT NULL,
+                    note_id INTEGER NOT NULL,
+                    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    note TEXT NOT NULL,
+                    FOREIGN KEY (user_id) REFERENCES users(id)
+                );"""
+        cursor.execute(query)
+        print("The 'notes' table has been created...")
+    except Error as e:
+        print(e)
+
+# Define a function to hash the password
+def hash_password(password):
+    # Convert the password to bytes and hash it using SHA256
+    password_bytes = password.encode('utf-8')
+    hashed_bytes = hashlib.sha256(password_bytes).digest()
+
+    # Convert the hashed bytes back to a string and return it
+    hashed_password = hashed_bytes.hex()
+    return hashed_password
+
+
+def username_exists(username):
+    # Connect to the database and check if a row exists in the 'users' table with the given username
+    conn = sqlite3.connect(DATABASE_LOCATION)
+    c = conn.cursor()
+    c.execute('SELECT * FROM users WHERE username = ?', (username,))
+    result = c.fetchone()
+
+    # Close the connection and return True if a row was found, otherwise return False
+    conn.close()
+    return result is not None
+
+def insert_user(username, password):
+    hashed_password = hash_password(password)
+
+    if username_exists(username):
+        return False
+
+    conn = sqlite3.connect('/home/gridl0ck/proj/online_notepad/note_app.db')
+    c = conn.cursor()
+
+    c.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, hashed_password))
+
+    conn.commit()
+    conn.close()
+
+def authenticate_user(username, password):
+    # Hash the password
+    hashed_password = hash_password(password)
+
+    conn = sqlite3.connect(DATABASE_LOCATION)
+    c = conn.cursor()
+    c.execute('SELECT password, id FROM users WHERE username = ?', (username,))
+    result = c.fetchone()
+
+    # If no result was found for the given username, return False
+    if not result:
+        return (False, -1)
+
+    # If a result was found, retrieve the hashed password and compare it to the entered password
+    stored_password = result[0]
+    user_id = result[1]
+    authenticated = hashed_password == stored_password
+
+    # Close the connection and return the authentication result
+    conn.close()
+    return (authenticated, user_id)
+
+if __name__ == "__main__":
+    print("This is not mean to be run standalone")
